4118b35ce2333a77e8e2cc5b8ad0ed486d06397c
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-04-13 17:03:50 -0400

    Get firebase expenses on start

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,11 +1,11 @@
-const promise = new Promise((resovle, reject) => {
+const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resovle({
-      name: 'Thomas',
-      age: 25
+    resolve({
+      name: 'Andrew',
+      age: 26
     });
     // reject('Something went wrong!');
-  }, 1500)
+  }, 5000);
 });
 
 console.log('before');
@@ -13,11 +13,15 @@ console.log('before');
 promise.then((data) => {
   console.log('1', data);
 
-  return 'some data';
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise');
+    }, 5000);
+  });
 }).then((str) => {
   console.log('does this run?', str);
 }).catch((error) => {
-  console.log('error', error);
+  console.log('error: ', error);
 });
 
 console.log('after');
