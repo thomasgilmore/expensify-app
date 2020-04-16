ebb9c1c7a88ebd87996fc3195a80b56c216ec1e9
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-04-16 12:57:39 -0400

    Add Expensify styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -54,18 +54,19 @@ export default class ExpenseForm extends React.Component {
     }
   };
   render() {
-    return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+    return (   
+        <form className="form" onSubmit={this.onSubmit}>
+          {this.state.error && <p className="form__error">{this.state.error}</p>}
           <input
             type="text"
             placeholder="Description"
             autoFocus
+            className="text-input"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
           <input
+            className="text-input"
             type="text"
             placeholder="Amount"
             value={this.state.amount}
@@ -81,13 +82,15 @@ export default class ExpenseForm extends React.Component {
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            className="textarea"
             value={this.state.note}
             onChange={this.onNoteChange}
           >
           </textarea>
-          <button>Add Expense</button>
+          <div>
+            <button className="button">Save Expense</button>
+          </div>
         </form>
-      </div>
     )
   }
 }
